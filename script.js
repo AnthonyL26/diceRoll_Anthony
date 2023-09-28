d24289073f7ecbf1bece1d17e1985210e6b65711
Your Name <you@example.com>
2023-09-28 10:42:33 -0400

    2d

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function getRandomInt() {
-    return Math.floor(Math.random() * 6);
+    return Math.floor(Math.random() * 6) +1;
   }
 
-var dice1 = document.getElementsByName("1");
-var dice2 = document.getElementsByName("2");
+  function rollDice() {
+    const dice1 = document.getElementById("1");
+    const dice2 = document.getElementById("2");
+    var num1 = getRandomInt();
+    var num2 = getRandomInt();
+    dice1.innerHTML = num1;
+    dice2.innerHTML = num2;
+
+
+}
+
 
 const button = document.querySelector("button");
 button.addEventListener("click", rollDice);
 
-function rollDice() {
-    
-    var num1 = getRandomInt();
-    var num2 = getRandomInt();
-    dice1.src = "Dice " +  `${num1}` + ".jpg";
-    dice2.src = "Dice " +  `${num2}` + ".jpg";
-}
